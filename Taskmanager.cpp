374a18c69ed17f22880974f7b4dfa7cf9975d21e
iKiks <ikiks.lanre@gmail.com>
2026-01-17 10:33:44 +0100

    qt tutorial

--- Taskmanager.cpp
+++ Taskmanager.cpp
@@ -1,3 +1,25 @@
-#include "Taskmanager.h"
+#include "TaskManager.h"
 
-TaskManager::TaskManager() {}
+TaskManager::TaskManager(QObject *parent)
+    : QObject{parent}
+{}
+
+void TaskManager::addTask(Task *task)
+{
+    task->setParent(this);
+    connect(task, &Task::taskCompleted, this, &TaskManager::onTaskCompleted);
+
+    m_tasks.append(task);
+}
+
+void TaskManager::completeAllTasks()
+{
+    for (Task* task : m_tasks) {
+        task->complete();
+    }
+}
+
+void TaskManager::onTaskCompleted(const QString &description)
+{
+    qInfo() << "[TaskManager] task completed: " << description;
+}
